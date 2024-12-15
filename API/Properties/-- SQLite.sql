4f8b3f7cf5035c15388c6a83870403da898c8585
garvn10 <garvnandwana10@gmail.com>
2024-12-15 15:37:50 +0530

    End of Section 2

---
+++ API/Properties/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (ID, UserName)
+VALUES (4, 'Grv');
+
+INSERT INTO Users (ID, UserName)
+VALUES (5, 'Lat');
+
+INSERT INTO Users (ID, UserName)
+VALUES (6, 'hat');
---
+++ API/entities/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API.entities;
+
+public class AppUser
+{
+   public int ID { get; set; }
+   public required String UserName { get; set; }
+}
